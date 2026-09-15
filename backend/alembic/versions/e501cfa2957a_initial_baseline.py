3709dc7e551f3743806ed359958e37d691829f43
IvanBozoCa <133916421+IvanBozoCa@users.noreply.github.com>
2026-09-15 10:38:06 -0300

    chore: initialize backend local-first foundation

---
+++ backend/alembic/versions/e501cfa2957a_initial_baseline.py
@@ -0,0 +1,28 @@
+"""initial baseline
+
+Revision ID: e501cfa2957a
+Revises: 
+Create Date: 2026-09-15 10:20:35.918729
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'e501cfa2957a'
+down_revision: Union[str, Sequence[str], None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass
